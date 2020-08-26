2ca48ed7c585002cf8a29f7cab90d6a30e158070
aysikh <amy.sikhammountry@gmail.com>
2020-08-26 13:11:37 -0400

    worked on first three issues

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,14 @@
+class CashRegister
+    attr_accessor :total, :discount, :title, :price
+    def initialize(discount = 0)
+        @total = 0
+        @discount = discount
+        @title = title
+        @price  price
+    end
+    def add_item
+        @title 
+        @price
+    end
+end
 
