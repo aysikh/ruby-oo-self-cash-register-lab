77eb49193a2ff49ade1c60bc98a148f3e6e93487
alexstephane <alexngundji@gmail.comm.com>
2020-08-26 16:09:01 -0400

    wip

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,14 +1,23 @@
+require 'pry'
+
 class CashRegister
-    attr_accessor :total, :discount, :title, :price
-    def initialize(discount = 0)
+    attr_accessor :total, :discount
+    def initialize(discount = 0, price =0 )
         @total = 0
         @discount = discount
-        @title = title
-        @price  price
+        @price = price
+        
+
+        
     end
-    def add_item
-        @title 
-        @price
-    end
+
+
+    def add_item(title,price ,quantity = 1)
+        @total += price * quantity 
+    
+        end                                                          
+
+     def apply_discount
+     end
 end
 
